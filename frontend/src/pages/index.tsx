7504da5d45b6697ab06c4ffd947d7a53e1019d85
HassanYusuf1 <112115781+HassanYusuf1@users.noreply.github.com>
2025-02-06 15:13:10 +0100

    push

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,10 +1,7 @@
 import Layout from "../shared/layout";
 
 export default function Home() {
-<<<<<<< HEAD
-  return <h1>Hei</h1>;
-}
-=======
+
   return (
     <Layout>
       <h2>Welcome to ISA DeepData</h2>
@@ -12,4 +9,4 @@ export default function Home() {
     </Layout>
   );
 }
->>>>>>> 02aebbcb3e975fb9f02c99df4f4f9e4d11badc0b
+
