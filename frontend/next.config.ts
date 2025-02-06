811f47cda999bd831c53f783d275edf1547aebf3
HassanYusuf1 <112115781+HassanYusuf1@users.noreply.github.com>
2025-02-06 13:50:22 +0100

    h
    d

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  srcDir: "src", // Forteller Next.js at src/ er root
 };
 
 export default nextConfig;
