83f55957cb2bcd5ba184d77b31f5622fdf3d5f81
mikailb <115622678+mikailb@users.noreply.github.com>
2025-02-06 15:40:42 +0100

    fiksa layout

--- frontend/src/shared/header.tsx
+++ frontend/src/shared/header.tsx
@@ -1,14 +1,20 @@
+import Image from "next/image";
 import Navigation from "../components/navigation/navigation";
 
 export default function Header() {
   return (
     <header className="header">
-      <div className="logo">
-        <img src="image/image.png" alt="Logo" />
-        <h1>ISA DeepData</h1>
-        <p>International Seabed Authority</p>
+      <div className="logo-container">
+        <Image src="/image/image.png" alt="Logo" width={80} height={80} className="logo-img" />
+        <div className="logo-text">
+          <h1>ISA DeepData</h1>
+          <p>International Seabed Authority</p>
+        </div>
       </div>
-      <Navigation />
+      {/* Plasser navbar under logoen */}
+      <div className="navbar-container">
+        <Navigation />
+      </div>
     </header>
   );
 }
