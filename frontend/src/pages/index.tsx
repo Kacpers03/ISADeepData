c99e68c2dca8a4823a17b4aa0a96d93d59a14c71
mikailb <115622678+mikailb@users.noreply.github.com>
2025-02-11 14:12:28 +0100

    video

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,15 +1,19 @@
-import Layout from "../shared/layout";
+import styles from "../styles/home.module.css";
 
 export default function Home() {
-
   return (
-     <>
-      <h2>Welcome to ISA DeepData</h2>
-      <p>Explore the ocean s depths with our interactive maps and resources.</p>
-      </>
-    
-
-    
+    // Wrapper for å bryte ut av Layouts container
+    <div className={styles.homeWrapper}>
+      <div className={styles.homeContainer}>
+        <video autoPlay muted loop className={styles.heroVideo}>
+          <source src="/video/ocean.mp4" type="video/mp4" />
+          Din nettleser støtter ikke videoavspilling.
+        </video>
+        <div className={styles.overlay}>
+          <h2>Welcome to ISA DeepData</h2>
+          <p>Explore the ocean's depths with our interactive maps and resources.</p>
+        </div>
+      </div>
+    </div>
   );
 }
-
