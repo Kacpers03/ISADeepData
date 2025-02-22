333b521805a24272c972dfe9037a5c7f150dd126
HassanYusuf1 <112115781+HassanYusuf1@users.noreply.github.com>
2025-02-22 13:36:25 +0100

    Databasen
    Endring

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
+import withTM from "next-transpile-modules";
 
-const nextConfig = {
-  // srcDir: "src", // Forteller Next.js at src/ er root
+const nextConfig: NextConfig = {
+  webpack: (config, { isServer }) => {
+    // TypeScript vet at config er av typen 'webpack.Configuration'
+    // Du kan eventuelt definere config som (config: any) hvis du vil være fleksibel
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "mapbox-gl": "maplibre-gl",
+    };
+    return config;
+  },
 };
 
-export default nextConfig;
+export default withTM(["react-map-gl"])(nextConfig);
