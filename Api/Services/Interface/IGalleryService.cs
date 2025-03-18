3d825e65af14b541d7f6a325641017ef213b9ea1
Kacpers03 <114741654+Kacpers03@users.noreply.github.com>
2025-03-18 13:57:39 +0100

    Databasen for gallery

---
+++ Api/Services/Interface/IGalleryService.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using DTOs.Gallery_Dto;
+
+namespace Api.Services.Interfaces
+{
+    public interface IGalleryService
+    {
+        Task<IEnumerable<GalleryItemDto>> GetGalleryItemsAsync(
+            int? contractorId, 
+            int? areaId, 
+            int? blockId, 
+            int? cruiseId, 
+            int? stationId, 
+            int? sampleId, 
+            string mediaType,
+            int? year);
+        
+        Task<(Stream Content, string ContentType, string FileName)> GetMediaForDownloadAsync(int mediaId);
+    }
+}
